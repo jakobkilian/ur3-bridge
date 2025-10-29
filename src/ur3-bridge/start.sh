1819ec37d58fe08e46fcbad461c1fbf0aa637484
Jakob Kilian <mail@jakobkilian.de>
2025-10-29 13:49:41 +0100

    make service call more portable

--- src/ur3-bridge/start.sh
+++ src/ur3-bridge/start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-source activate ur3-bridge
 
 # Get the directory of the current script
 SCRIPT_DIR=$(dirname "$0")
+# Dynamically extract the username from SCRIPT_DIR
+USER_NAME=$(echo "$SCRIPT_DIR" | awk -F'/' '{print $3}')
 
-# Construct the path to str_over_socket.py
+# Construct the path to ur3-bridge.py
 PYTHON_SCRIPT_PATH="${SCRIPT_DIR}/ur3-bridge.py"
 
-# Run the python script
-exec python "$PYTHON_SCRIPT_PATH"
+# Set the path to the Python executable in the Conda environment
+PYTHON_EXE="/home/$USER_NAME/miniforge3/envs/ur3-bridge/bin/python"
+
+# Run the python script using the full path to the environment's python
+exec "$PYTHON_EXE" "$PYTHON_SCRIPT_PATH"
